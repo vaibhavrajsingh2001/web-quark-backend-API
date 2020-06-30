2e0942dfcce8ca38d2ae1e8027e36191551b03a1
vaibhavrajsingh2001 <vaibhavrajsingh2001@gmail.com>
2020-06-30 16:07:19 +0530

    added edit & delete quiz endpoints. Ready for deployment.

--- src/routes/quizzes.js
+++ src/routes/quizzes.js
@@ -23,6 +23,11 @@ quizRouter.get('/', async (req, res) => {
 quizRouter.get('/:id', async (req, res) => {
     try {
         const quiz = await Quiz.findById(req.params.id);
+
+        if (!quiz) {
+            return res.status(404).json({ msg: 'no such quiz found!' });
+        }
+
         res.json(quiz);
     } catch (err) {
         console.error(err.message);
@@ -60,5 +65,60 @@ quizRouter.post(
         }
     }
 );
+
+// edit a quiz
+quizRouter.put('/:id', auth, async (req, res) => {
+    const { name, quizData } = req.body;
+
+    const quizStructure = {};
+
+    if (name) quizStructure.name = name;
+    if (quizData) quizStructure.quizData = quizData;
+
+    try {
+        let quiz = await Quiz.findById(req.params.id);
+
+        // check if the quiz of that id exists
+        if (!quiz) {
+            return res.status(404).json({ msg: 'no such quiz found!' });
+        }
+        // check authentication that the owner of the quiz is editing
+        if (quiz.user.toString() !== req.user.id) {
+            return res.status(400).json({ msg: 'unauthorized access!' });
+        }
+
+        // save the new quiz
+        quiz = await Quiz.findByIdAndUpdate(req.params.id, {
+            $set: quizStructure,
+        });
+
+        res.json(quiz);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+// delete a quiz
+quizRouter.delete('/:id', auth, async (req, res) => {
+    try {
+        let quiz = await Quiz.findById(req.params.id);
+
+        if (!quiz) {
+            return res.status(404).json({ msg: 'no such quiz found!' });
+        }
+        if (quiz.user.toString() !== req.user.id) {
+            return res.status(400).json({ msg: 'unauthorized access!' });
+        }
+
+        // delete the quiz
+        await Quiz.findByIdAndRemove(req.params.id);
+
+        res.json({ msg: 'quiz has been removed' });
+    } catch (err) {
+        console.err(err.message);
+        res.status(500).send('server error');
+    }
+});
 
 module.exports = quizRouter;
